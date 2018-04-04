80a2ea21284555eda9b36ce9e974cba157fd645f
Wayne <Wayne.Goosen@Derivco.co.za>
2018-04-04 21:24:05 +0200

    Moved files. WIP: cater for different end of line sequences and escape double quotes.

--- GenerateSnippet/GenerateSnippet.py
+++ GenerateSnippet/GenerateSnippet.py
@@ -2,7 +2,15 @@ import pyperclip
 
 content = pyperclip.paste()
 
-lines = content.split('\n')
+cr = '\r'
+lf = '\n'
+crlf = cr + lf
+
+endOfLineSequence = crlf
+
+lines = content.split(endOfLineSequence)
+
+escapeDoubleQuotes = '\\"'
 
 # tab size of the source code you are copying from
 tabSize = 4
@@ -10,18 +18,19 @@ tabIdentifier = tabSize * ' '
 
 snippetLines = []
 for line in lines:
+	line = line.replace('"', escapeDoubleQuotes)
 	snippetLines.append('"' + line.replace(tabIdentifier, '\\t') + '"')
 
-separator = ',\n\t\t\t'
+bodyLineseparator = ',\n\t\t\t'
 
-body = separator.join(snippetLines)
+body = bodyLineseparator.join(snippetLines)
 
 template = '''"KEY": {
 		"prefix": "",
+		"description": "",
 		"body": [
 			#TOKEN#
-		],
-		"description": ""
+		]
 	},'''
 
 snippet = template.replace('#TOKEN#', body)
